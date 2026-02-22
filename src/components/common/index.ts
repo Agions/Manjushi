7ffd06aa1dfb4fa78424f929a666a09e4e2d32c9
agions <1051736049@qq.com>
2026-02-22 23:40:39 +0800

    feat: 添加 Toast 通知组件
    - toast 快捷消息提示
    - notify 带描述的通知
    - closeAll 关闭所有通知

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -5,3 +5,4 @@
 export { default as Loading } from './Loading';
 export { default as EmptyState } from './Empty';
 export { default as PageHeader } from './PageHeader';
+export { default as toast, notify, closeAll } from './Toast';
