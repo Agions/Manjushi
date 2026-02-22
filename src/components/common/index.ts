aeece3028556b4535f73d36c5cb202433441851d
agions <1051736049@qq.com>
2026-02-22 23:38:51 +0800

    feat: 添加通用 UI 组件
    - Loading 加载组件 (全屏/骨架屏)
    - EmptyState 空状态组件 (项目/文件类型)
    - PageHeader 页面头部 (面包屑/返回按钮)

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,7 +1,7 @@
 /**
- * 通用组件统一导出
+ * 通用组件导出
  */
 
-// 基础组件
-export { default as Card } from './Card';
-export { default as Button } from './Button';
+export { default as Loading } from './Loading';
+export { default as EmptyState } from './Empty';
+export { default as PageHeader } from './PageHeader';
